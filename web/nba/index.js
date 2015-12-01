8af8d53961a5eec0d804b9f9578c48fc1eac65c2
tyrannothesauruslex <tyrannothesauruslex@gmail.com>
2015-12-01 12:38:22 -0800

    get info by click on line (as well as mouseover point)

--- web/nba/index.js
+++ web/nba/index.js
@@ -291,13 +291,12 @@ function chartPlayerStats(name, pid, color1, color2) {
           })
           .attr("d", line)
           .on("click", function(d){
-            console.log(d);
             d3.selectAll("text#tip").remove();
             d3.select("#chart")
                 .append("text")
-                .text(d.PTS_ave + ", " + d.MIN_ave + "min " + d.name + " | " + d.PTS + "pts " + d.game  )
-                .attr("x", xScale( parseDate(d.GAME_DATE)) + 10)
-                .attr("y", yScale(d.PTS_ave) - 1)
+                .text(d[0].PTS_ave + ", " + d[0].MIN_ave + "min " + d[0].name + " | " + d[0].PTS + "pts " + d[0].game  )
+                .attr("x", xScale( parseDate(d[0].GAME_DATE)) + 10)
+                .attr("y", yScale(d[0].PTS_ave) - 1)
                 //.attr("id", d.line_id)
                 .attr("id", "tip")
                 ;
