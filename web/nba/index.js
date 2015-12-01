e7df2fae607b262c0ff058abb1906bb7655c4f6e
tyrannothesauruslex <tyrannothesauruslex@gmail.com>
2015-12-01 15:51:32 -0800

    change text to html

--- web/nba/index.js
+++ web/nba/index.js
@@ -355,8 +355,8 @@ function addHammerEventListener(that, d){
             .text(d.PTS_ave + " " + d.name + " (" + d.MIN_ave + "mpg) " +" | " + d.PTS + "pts " + d.game  )
             //.attr("x", xScale( parseDate(d.GAME_DATE)) - 10)
             //.attr("y", yScale(d.PTS_ave) - 1)
-            .style("top", (event.pageY + 16) + "px")
-            .style("left", (event.pageX + 16) + "px")
+            .style("top", (event.center.y + 16) + "px")
+            .style("left", (event.center.x + 16) + "px")
             .attr("paint-order", "stroke")
             //.attr("id", d.line_id)
             ;
