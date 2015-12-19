410a6b644280b22981e0b9e6497fc69751bdca83
tyrannothesauruslex <tyrannothesauruslex@gmail.com>
2015-12-19 15:41:49 -0800

    debug ode_endpoint

--- web/server/ode_endpoint.php
+++ web/server/ode_endpoint.php
@@ -22,7 +22,7 @@ CREATE TABLE comments (word TEXT, comment_date DATE NOT NULL DEFAULT CURRENT_DAT
 CREATE TABLE comments (word TEXT, comment TEXT);
 
 */
-include("$_SERVER[DOCUMENT_ROOT]/config.php");
+include("$_SERVER[DOCUMENT_ROOT]/server/config.php");
 $word = $_REQUEST["term"];
 
 try {
