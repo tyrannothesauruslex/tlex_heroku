67048a099c0c29c450191886e1af834dc0b28e26
tyrannothesauruslex <tyrannothesauruslex@gmail.com>
2015-12-19 18:34:34 -0800

    rethink design pattern

--- web/server/config.php
+++ web/server/config.php
@@ -4,5 +4,8 @@ $HOST = 'ec2-54-225-197-30.compute-1.amazonaws.com';
 $USER = 'dzjaebpbglolem';
 $DB_NAME = 'd5rpi1j0et2mpo';
 $PASSWORD = 'aGciQGgo9x7FtTWdFh-0gWW6du';
-
+/*
+psql -h <host> -p <port> -U <username> -W <password> <database>
+psql -h <host> -p <port> -U <username> -W <password> <database>
+*/
 ?>
--- web/server/ode_endpoint.php
+++ web/server/ode_endpoint.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8" />
+    <title>TyrannoThesaurusLex</title>
+    <meta name='viewport' content='initial-scale=1,user-scalable=yes' />
+
+        <link rel="stylesheet" href="../assets/normalize.css">
+        <link rel="stylesheet" href="../assets/style.css">
+        <!-- <link href='assets/google_fonts.css' rel='stylesheet' type='text/css'> -->
+        <!-- <link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet' type='text/css'> -->
+        <link href="../assets/font-awesome-4-2-0.min.css" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700|Satisfy' rel='stylesheet' type='text/css'>
+
+        <script src="../assets/jquery-1.9.1.min.js"></script>
+        <style>
+
+        </style>
+    </head>
+<body>
+
+
 <?php
 // This IS the homage+comments page.
 
@@ -15,75 +37,98 @@ DB design:
         term2, comment_b, comment_date
         term2, comment_c, comment_date
 
-<<<<<<< d145bd9d59ef5affa1455118e44d3a876b4c058f
 CREATE TABLE terms (word TEXT PRIMARY KEY NOT NULL, date_added DATE NOT NULL DEFAULT CURRENT_DATE);
 CREATE TABLE comments (word TEXT, comment_date DATE NOT NULL DEFAULT CURRENT_DATE, comment TEXT);
 
 CREATE TABLE comments (word TEXT, comment TEXT);
+ALTER TABLE comments ADD COLUMN example TEXT;
 
+IFF the word is not yet in the terms table, insert it.
+Otherwise, don't insert it, but display existing example/comments and prompt user for a new ones.
 */
 include("$_SERVER[DOCUMENT_ROOT]/server/config.php");
-$word = $_REQUEST["term"];
+if(isset($_REQUEST['term']) && $_REQUEST['term']!="") {
+    $word = $_REQUEST["term"];
+}
 
-try {
+?>
+
+<h3 id="the_word"><?= $word ?></h3>
+<textarea id="new_ode_example" name="new_ode_example" placeholder="word in the wild" style="width:100%; height:0.5in;"></textarea>
+<textarea id="new_ode_comment" name="new_ode_comment" placeholder="comment" style="width:100%; height:0.5in;"></textarea>
+<a href="javascript:void(0)" id="ode_save" class="button-b">Save</a>
+
+<!--
+"Here's to the Halcyon, forever may she rest"
+Old 97s, Here's to the Halcyon, Blame it on Gravity
+ -->
+
+<script>
+
+    $('#ode_save').click(function(){
+        // send it to the php insert endpoint
+        var data = {
+            "the_word": $('#the_word').text(),
+            "new_ode_example": $('#new_ode_example').val(),
+            "new_ode_comment": $('#new_ode_comment').val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "ode_endpoint.php",
+            data: data
+        }).done(function( msg ) {
+            //console.log( "Data Saved: " + msg );
+            console.log( JSON.stringify(msg) );
+        });
+    });
+</script>
+
+<?php
+/*
+echo "<h3>$word</h3>\n";
+echo '<textarea id="new_ode_example" placeholder="word in the wild" style="width:100%; height:0.5in;"></textarea>';
+echo '<textarea id="new_ode_comment" placeholder="comment" style="width:100%; height:0.5in;"></textarea>';
+echo '<a href="javascript:void(0)" id="ode_save" class="button-b">Save</a>';
+*/
+//try {
     //$dbh = new PDO("pgsql:host='localhost';dbname=$DB_NAME", $USER, $PASSWORD);
     $dbh = new PDO("pgsql:host='$HOST';dbname=$DB_NAME", $USER, $PASSWORD);
 
-    // INSERT INTO terms (word) VALUES ('moxie');
-    $sql = "INSERT INTO terms (word) VALUES ('$word')";
+    //if ($_POST) {
+    if( isset($_REQUEST['the_word']) && isset($_REQUEST['new_ode_example']) && isset($_REQUEST['new_ode_comment'])  ) {
 
-    $inserted = $dbh->exec( $sql );
+        $the_word=$_POST['the_word'];
+        $new_ode_example=$_POST['new_ode_example'];
+        $new_ode_comment=$_POST['new_ode_comment'];
+
+        $sql = "INSERT INTO comments (word, example, comment) VALUES ('$the_word', '$new_ode_example, '$new_ode_comment)";
+        $inserted = $dbh->exec( $sql );
+    }
 
 
-    $sql = "SELECT word, date_added FROM terms WHERE word = '$word'";
+
+    $sql = "SELECT word, example, comment FROM comments WHERE word = '$word'";
 
     $result = $dbh->query($sql);
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         echo "<tr>";
         echo "<td>" . $row["word"] . "</td>";
-        echo "<td>" . htmlspecialchars($row["date_added"]) . "</td>";
+        echo "<td>" . htmlspecialchars($row["example"]) . "</td>";
+        echo "<td>" . htmlspecialchars($row["comment"]) . "</td>";
         echo "</tr>";
     }
-    echo $sql;
-    echo $inserted;
-}
-catch(PDOException $e) {
+//}
+/*catch(PDOException $e) {
     echo $e->getMessage();
 }
-
-/*
-$query = "SELECT COUNT(*) FROM tlex WHERE term = $word";
-=======
 */
-$word = $_REQUEST["term"];
-
-$dbconn = pg_connect("host=www.tyrannothesauruslex.com dbname=$db user=pgsql password=") or die('Could not connect: ' . pg_last_error());
-
-$query = "SELECT COUNT(*) FROM tlex WHERE term = $word";
-//echo "<br>alter query: $query\n";
-$result = pg_query($dbconn, $query);
-
-/*
-if ($result????row = 0) {
-    $insert = "INSERT $word, date INTO tlex(term, date)";
-    pg_query($dbconn, $insert);
-}
-*/
-// Show the word, def, syns:
-
-
-// Now check for previous entries:
-$query = "SELECT COUNT(*) FROM tlex WHERE term = $word";
-//echo "<br>alter query: $query\n";
-$result = pg_query($dbconn, $query);
-
-
-
 
 
 ?>
 
 
-
+</body>
+</html>
 
 
